a1b1156632ea5d2a0843938d85ea0986f77955be
Oussama <Hassouneoussama8@gmail.com>
2025-07-05 12:08:13 +1000

    start with Assembly

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import Key from './Key.jsx'
+
+function App() {
+
+  return (
+    <>
+    <div className='container'>
+      <Key/>
+      <Key/>
+      <Key/>
+      <Key/>
+      <Key/>
+      <Key/>
+      <Key/>
+      <Key/>
+      <Key/>
+      <Key/>
+      <Key/>
+      <Key/>
+      <Key/>
+      <Key/>
+      <Key/>
+      <Key/>
+      <Key/>
+      <Key/>
+      <Key/>
+      <Key/>
+      <Key/>
+      <Key/>
+      <Key/>
+      <Key/>
+      <Key/>
+    </div>
+
+    </>
+  )
+}
+
+export default App
